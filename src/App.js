4593a00632447dff7d86c5b019f3c26c0227d35e
Ariell SCott <scott.ariell00@gmail.com>
2021-12-08 19:10:09 -0500

    file structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import About from './components/About';
+
+
+function App() {
+  return (
+  <h2>Portfolio Project..</h2>
+  );
+}
+
+export default App;
